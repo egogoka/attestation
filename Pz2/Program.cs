19c7ee31d7211b5036acf04ebc72e21d17aa1e15
Lilia <popik7703@gmail.com>
2024-06-24 18:58:05 +0300

    add pz2

---
+++ Pz2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+Console.Clear();
+int Prompt(string message)
+{
+    Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+    else return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+int m = Prompt("Введите M: ");
+int n = Prompt("Введите N: ");
+
+Console.WriteLine($"A({m},{n}) = {Akkerman(m, n)}");
