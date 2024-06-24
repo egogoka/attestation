d192641f7d64d086b789c93a72bc1a68457f55e5
Lilia <popik7703@gmail.com>
2024-06-24 19:32:47 +0300

    add pz3

---
+++ Pz3/Program.cs
@@ -0,0 +1,20 @@
+﻿// // Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// // Использовать рекурсию, не использовать циклы..
+
+
+int[] array = {10,2,5,4,5,6,6};
+int iter = 0;
+      
+PrintArr(array,iter);
+			
+static void PrintArr(int[] array, int i)
+    {
+	  if (i >= array.Length) 
+      {
+	    return;
+	  }
+	  PrintArr(array,i+1);
+	  Console.Write(array[i]+" ");
+	}
+		
+	
