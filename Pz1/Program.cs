0d282a3e79529555de838f202d9c6b7544a515f5
Lilia <popik7703@gmail.com>
2024-06-24 18:46:14 +0300

    add pz1

---
+++ Pz1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа
+// в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение M: ");
+        int M = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        PrintNaturalNumbers(M, N);
+    }
+
+    static void PrintNaturalNumbers(int current, int N)
+    {
+        if (current <= N)
+        {
+            Console.Write(current + " ");
+            PrintNaturalNumbers(current + 1, N);
+        }
+    }
+}
